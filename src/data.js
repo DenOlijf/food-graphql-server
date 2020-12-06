c1465f6f2b01b7ea62465dcb2e4b28ebd3c49c56
OlivierVdN <olivier.vandennest@ugent.be>
2020-12-06 21:10:19 +0100

    fix const stores error

--- src/data.js
+++ src/data.js
@@ -2,8 +2,8 @@ const uuid = require('uuid');
 
 const stores = [
   {
-    id: 'cc406ed9-fc02-4185-b073-8c12b61b5c79'
-    name: 'Den Olijfboom',
+    id: 'cc406ed9-fc02-4185-b073-8c12b61b5c79',
+    name: 'Den Olijfboom'
   },
   {
     id: '5f2919aa-333a-4745-8166-3002ab30de0e',
